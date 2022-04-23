eb6498c32464b89fc10bf614dabc6ca1141b9903
parsaklgr <parsaklgr@gmail.com>
2022-04-23 17:17:28 +0430

    revised by parsa

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require('multer')
 
+const upload = multer({ dest: 'uploads/' })
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res, next) {
+  // req.file is the `avatar` file
+  // req.body will hold the text fields, if there were any
+  console.log(Object.keys(req.file));
+  res.json({ name: req.file.originalname, type: req.file.mimetype, size: req.file.size });
+  next();
+})
 
 
 const port = process.env.PORT || 3000;
